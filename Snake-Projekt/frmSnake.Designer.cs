395fd7412966beef945052d83307d04203ccb21f
NiklasHeckmann <niklas.heckmann@gmail.com>
2017-02-20 09:03:36 +0100

    hi

--- Snake-Projekt/frmSnake.Designer.cs
+++ Snake-Projekt/frmSnake.Designer.cs
@@ -28,34 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.pbSpielFeld = new System.Windows.Forms.PictureBox();
+            this.timZeit = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpielFeld)).BeginInit();
             this.SuspendLayout();
             // 
-            // pictureBox1
+            // pbSpielFeld
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(524, 362);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pbSpielFeld.Location = new System.Drawing.Point(0, 0);
+            this.pbSpielFeld.Name = "pbSpielFeld";
+            this.pbSpielFeld.Size = new System.Drawing.Size(524, 362);
+            this.pbSpielFeld.TabIndex = 0;
+            this.pbSpielFeld.TabStop = false;
+            this.pbSpielFeld.Click += new System.EventHandler(this.pbSpielFeld_Click);
+            // 
+            // timZeit
+            // 
+            this.timZeit.Tick += new System.EventHandler(this.timZeit_Tick);
             // 
             // frmGame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(747, 361);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pbSpielFeld);
             this.Name = "frmGame";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Text = "Snake";
+            this.Load += new System.EventHandler(this.frmGame_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbSpielFeld)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbSpielFeld;
+        private System.Windows.Forms.Timer timZeit;
     }
 }
 
